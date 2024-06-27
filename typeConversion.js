bc91d8765cdad445988f68f516a6e5a95a30f34b
Rishav Raj <139668894+Rishav-R03@users.noreply.github.com>
2024-06-27 11:21:26 +0000

    typeConversion

---
+++ typeConversion.js
@@ -0,0 +1,11 @@
+//string to int and vice-versa
+
+let score = "33"
+// let name = 
+
+console.log(typeof score)
+let to_int = Number(score)
+console.log(typeof to_int)
+console.log(to_int)
+
+// let to_string = String()
