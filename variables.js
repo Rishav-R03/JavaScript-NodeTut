6861419a76bbdbe5f76b9086027672fe998a378b
Rishav Raj <139668894+Rishav-R03@users.noreply.github.com>
2024-06-27 11:04:32 +0000

    variables in javascript

--- variables.js
+++ variables.js
@@ -1,3 +1,17 @@
 /**
  * Prefer not to use var because of issue in block scope and functional scope.
+ * 
  */
+let fName = "Rishav"
+
+
+let lName = "Raj"
+const accountNo = 500434565544
+const mobNo = 9311571439
+
+console.table([fName,lName,accountNo,mobNo])
+
+let person = { // javascript object with attributes
+        name :"Rishav",
+        age : 21
+    }
