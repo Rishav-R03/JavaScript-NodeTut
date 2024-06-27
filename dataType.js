fdfc6e5c7d7835355a68abf67cf1bfb727384319
Rishav Raj <139668894+Rishav-R03@users.noreply.github.com>
2024-06-27 11:11:55 +0000

    dataTypes in JS

--- dataType.js
+++ dataType.js
@@ -1,2 +1,20 @@
 "use strict";// treat all JS code as latest ECMA or latest version
 // alert(40+3) we are using node.js to run code but not a browser
+
+/**
+ * number : 2 ^ 52 is range for number
+ * string 
+ * bigInt : trading stock 
+ * bool
+ * null : standalone value
+ * undefined : value is not assigned or given
+ * symbol : to create uniqueness in react components 
+ */
+
+/**
+ * objects 
+ * 
+ */
+console.log(typeof "rishav")
+console.log(typeof null) // it's and object
+console.log(typeof undefined) // it's undefined
