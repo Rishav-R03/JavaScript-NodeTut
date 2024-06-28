7cbbf2c725378f3f2034b4fddf47987d71a72a78
Rishav Raj <139668894+Rishav-R03@users.noreply.github.com>
2024-06-28 12:18:43 +0000

    strings and operations

---
+++ strings.js
@@ -0,0 +1,14 @@
+let value = 3
+let negVal = -value
+
+console.log(value); 
+console.log(negVal);
+
+/**
+ * operations : +,-,/,*,% **(exponential)
+ 
+*/
+let str1 = "hello"
+let str2 = " rishav"
+let greet = str1 + str2
+console.log(greet)
