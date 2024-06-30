7b5f3fff75458de3e4e247f5a780877b015d418f
Rishav-R03 <rishav042023@gmail.com>
2024-06-30 13:58:25 +0530

    String ops

--- strings.js
+++ strings.js
@@ -6,9 +6,42 @@ console.log(negVal);
 
 /**
  * operations : +,-,/,*,% **(exponential)
- 
 */
 let str1 = "hello"
 let str2 = " rishav"
 let greet = str1 + str2
 console.log(greet)
+
+console.log("1"+2+2) //if string is first then it will be treated as string
+console.log(2,4,"3") //here first is number it will be treated as int
+
+console.log(true)
+/**
+ * Basic Array Methods
+Array length
+Array toString()
+Array at()
+Array join()
+Array pop()
+Array push()
+
+See Also:
+Search Methods
+Sort Methods
+Iteration Methods	
+
+Array shift()
+Array unshift()
+Array delete()
+Array concat()
+Array copyWithin()
+Array flat()
+Array splice()
+Array toSpliced()
+Array slice()
+*/
+
+let gamecounter = 100
+gamecounter++;
+++gamecounter;
+console.log(gamecounter)
