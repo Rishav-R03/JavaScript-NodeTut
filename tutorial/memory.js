ddfbeb152aaa999258f40bed5515afe8434bff82
Rishav-R03 <rishav042023@gmail.com>
2024-08-11 16:06:35 +0530

    & 'e:\Web Practice\Chai with Javascript\JavaScript-NodeTut\tutorial\strings.js'

---
+++ tutorial/memory.js
@@ -0,0 +1,11 @@
+/**
+ * Stack and heap
+ * stack contains all primitive data types 
+ * heap contains all the non primitive data types.
+ */
+let userOne = { //ye jaiga heap
+    name: "userOne",
+    email: "userOne@gmail.com"
+}
+let name = "userName"; //jaiga stack
+
---
+++ tutorial/strings.js
@@ -0,0 +1,10 @@
+let greet = "hello" + " world";
+// console.log(greet + 50 + "from me"); outdated
+
+// console.log(`Hello My name is ${varName} and I am a prefinal year student at ${varName}`);
+
+const gameName = new String('rishav23')
+console.log(gameName)
+
+console.log(gameName[0]); 
+console.log(gameName.__proto__)
