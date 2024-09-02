20a03da7c6ef062d5326eb2cddb6018b067afade
Rishav Raj <139668894+Rishav-R03@users.noreply.github.com>
2024-09-02 05:26:34 +0000

    functions in javascript

--- functions.js
+++ functions.js
@@ -1,5 +1,22 @@
-function myfunc ()
-{
-    let a = 0;
-    let b = 0;
+// function myfunc ()
+// {
+//     let a = 0;
+//     let b = 0;
+// }
+
+// function addNum(num1,num2){
+//     if(num1===num2){
+//         console.log(num1+num2);
+//     }
+//     else{
+//         console.log("Invalid Data type for addtion");
+//     }
+// }
+
+function addNum(num1,num2){
+    let result = num1+num2;
+    console.log("Result", result);
+    return result;
 }
+addNum(3,4);
+// addNum(3,"a");
