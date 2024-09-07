57d7b08155cce1f211c95ae8b3ab885f8cbae35e
Rishav-R03 <rishav042023@gmail.com>
2024-09-07 16:13:15 +0530

    arrays in js

---
+++ arraysinJS.js
@@ -0,0 +1,12 @@
+//array
+//js arrays are resizable and can contain any data type such as string, number, boolean, object.
+//copying array creates shallow copies that means they are sharing same reference in memory. Change in one is visible to another.
+//deep copies that are not sharing same reference, change in one does not affect original array.
+const myArr = ["first index",1,2,3,4,5, "array",false];
+console.log(myArr[0]);
+const mylength = myArr.length;
+console.log(mylength);
+
+
+
+
