be1e3cfe09bc9c0e63c28a5572b9ec290374eed5
Rishav-R03 <rishav042023@gmail.com>
2024-09-11 22:01:22 +0530

    remove unused

--- tutorial/Multipage/myapp/src/AboutMe/AboutMe.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AboutMe = () => {
-  return (
-    <div>AboutMe Page</div>
-  )
-}
-
-export default AboutMe
--- tutorial/Multipage/myapp/src/Education/Education.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Education  = () => {
-  return (
-    <div>Education Details</div>
-  )
-}
-
-export default Education 
