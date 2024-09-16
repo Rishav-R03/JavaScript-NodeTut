564a1207eaabd43277a6d7d4d22c3408cbb7f772
Rishav-R03 <rishav042023@gmail.com>
2024-09-16 23:23:14 +0530

    destructring

---
+++ destructuringObject.js
@@ -0,0 +1,14 @@
+const course = 
+    {
+        name:"JavaScript",
+        price:1000,
+    }
+
+
+const {price:p} = course
+console.log(p);
+// console.log(price);
+
+//using the square bracket make array 
+//using the curly bracket make object
+//using curly bracket under square bracket make array of objects
