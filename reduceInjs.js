0e4c3208c163795d0682a83ffbbd501b77cbd737
Rishav-R03 <rishav042023@gmail.com>
2024-09-16 23:09:33 +0530

    reduce in js

--- reduceInjs.js
+++ reduceInjs.js
@@ -4,6 +4,28 @@ const myTotal = myNums.reduce((acc,currval)=>{
     return acc+currval
 },0);
 
-const myTotal1 = myNums.reduce((acc,currval)=>acc+currval,0);
+const myTotal1 = myNums.reduce((acc,currval)=>acc+currval,0); // 0 is the initial value of sum 
 console.log(myTotal);
 console.log(myTotal1);
+
+const shoppingCart = [
+    {
+        name:"Monitor",
+        price:35000
+    },
+    {
+        name:"Mouse",
+        price:1000
+    },
+    {
+        name:"Keyboard",
+        price:1500
+    },
+    {
+        name:"CPU",
+        price:350000
+    },
+];
+
+const shoppingTotal = shoppingCart.reduce((acc,item)=>acc+item.price,0);
+console.log(shoppingTotal);
