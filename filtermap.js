1f1df928e074852ff028e3efd38fa382eceb4ffb
Rishav-R03 <rishav042023@gmail.com>
2024-09-16 22:33:04 +0530

    filter and reduce in js

---
+++ filtermap.js
@@ -0,0 +1,22 @@
+const coding = ["js","cpp","py","java"];
+coding.forEach((item)=>{
+    console.log(item);
+})
+
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+const newNums = myNums.filter((item)=>item>4);
+// const newNums1 = myNums.filter((item)=>{return item>4});
+//using scope {} requires return keyword.
+console.log(newNums);
+// console.log(newNums1);
+
+//foreach
+const myTab = [2,4,6,8,10,12,14,16,18,20];
+const newTab = [];
+
+myTab.forEach((item)=>{
+    if(item>15){
+        newTab.push(item);
+    }
+})
+console.log(newTab)
