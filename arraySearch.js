6a65adbc809dba7752401aaa0218520d39853ce3
Rishav-R03 <rishav042023@gmail.com>
2024-09-19 21:52:04 +0530

    array search

---
+++ arraySearch.js
@@ -0,0 +1,11 @@
+/*
+ *Array Find and Search Methods
+Array indexOf()
+Array lastIndexOf()
+Array includes()
+
+Iteration Methods	Array find()
+Array findIndex()
+Array findLast()
+Array findLastIndex() 
+*/
