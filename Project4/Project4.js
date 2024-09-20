8e2cd2e17d95b0dc3bd34f5adac08bde21d6caca
Rishav-R03 <rishav042023@gmail.com>
2024-09-20 16:26:33 +0530

    number guessing game

---
+++ Project4/Project4.js
@@ -0,0 +1,27 @@
+const randomNum = parseInt((Math.random()*100 + 1));
+// console.log(random);
+const submit = document.querySelector('#btn');
+const userIn = document.querySelector('#number');
+const prevGuess = document.querySelector('#previous-guess');
+const NumAttempts = document.querySelector('#attempts');
+const result = document.querySelector('#result');
+const loworhigh = document.querySelector('#loworhigh');
+
+let prev_Guess = []
+let numGuess = 1;
+let playGame = true;
+
+function validateGuess(guess){
+    //check if it is int only and it is between 1-100
+
+}
+function checkGuess(guess){
+
+}
+function displayMessage(message){
+
+}
+
+function displayGuess(guess){
+    
+}
