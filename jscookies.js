c1a06a73087d9fd5dedc8189432f24d2a7fed360
Rishav-R03 <rishav042023@gmail.com>
2024-10-11 11:08:28 +0530

    js cookies

--- jscookies.js
+++ jscookies.js
@@ -8,3 +8,6 @@ let value = prompt("Enter your value");
 document.cookie = `${key} = ${value}`
 document.cookie = `${encodeURIComponent(key)} = ${encodeURIComponent(value)}` // {key} = ${value}`
 console.log(document.cookie)
+
+// cookies are stored in the form key=value
+//cookies are small strings in the form key=value
